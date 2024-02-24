32aabe8c37622043d8aa8626fd3d6b5d0ce0ca9b
namwaya <duncan.owino@gmail.com>
2024-02-24 14:29:49 +0000

    updates

---
+++ script.js
@@ -0,0 +1,12 @@
+// variable and output
+var message = "Hello, Javascript";
+console.log(message);
+
+//Functions. function takes argument names name and returns a msg
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+//Here we call the function
+var greeting = greet("World");
+console.log(greeting)
